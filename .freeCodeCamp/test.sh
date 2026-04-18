0a9cecde9fee4be6a9d6c22bc16bbbdbf9834a44
Tamene-eng <tameshim8@gmail.com>
2026-04-18 12:40:52 -0400

    new five programs complete project

---
+++ .freeCodeCamp/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Program to run my other four programsquestionnaire.sh
+./questionnaire.sh
+./countdown.sh a
+./bingo.sh
+./fortune.sh
---
+++ five.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Program to run my other four programsquestionnaire.sh
+./questionnaire.sh
+./countdown.sh 3
+./bingo.sh
+./fortune.sh
